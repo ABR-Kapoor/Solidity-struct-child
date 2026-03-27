e09a5a718424a20a8c51426629d051c1dec05992
Abeer Kapoor <101738668+ABR-Kapoor@users.noreply.github.com>
2026-03-27 15:40:13 +0530

    Update useConstractor.sol

--- useConstractor.sol
+++ useConstractor.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
 
+contract Wallet {
+    address sender;
+    uint valueSet;
+
+    function payContract() public payable {
+        sender = msg.sender;
+        valueSet = msg.value;
+    }
+}
+
+contract paymentReceived {
+    address public from;
+    uint public amt;
+}
